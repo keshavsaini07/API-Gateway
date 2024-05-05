cd8945066b8c650dceac57105d1f85cebda32d9a
keshavsaini07 <sainikeshav770@gmail.com>
2024-05-05 23:09:46 +0530

    Added rate limiter for apis

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 const express = require('express');
+const rateLimit = require('express-rate-limit');
 
 const {ServerConfig} = require('./config');
 const apiRoutes = require('./routes');
 
+const limiter = rateLimit({
+  windowMs: 2*60*100, // 2 minutes : window-time to reset
+  max: 3 // limits each ip to 3 requests
+})
+
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(limiter);
 
 app.use('/api', apiRoutes);
 
